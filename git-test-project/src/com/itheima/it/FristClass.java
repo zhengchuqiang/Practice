14afda8beb306cc81064b9933db3852abff18fd5
Shuiping <1300476531@qq.com>
2019-11-08 16:34:04 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/it/FristClass.java
@@ -0,0 +1,15 @@
+package com.itheima.it;
+
+public class FristClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+
+    public void test(){
+        int i =0;
+        i +=10;
+    }
+}
